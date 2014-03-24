9ccbcf8e701308463f2452615d7918010ce7b172
Trent Apted <tapted@chromium.org>
2014-03-24 13:30:45 +1100

    end-to-end working on Mac

--- polymer/requirements-mac.mk
+++ polymer/requirements-mac.mk
@@ -1,11 +1,13 @@
 ## If not in the path, try to find these in homebrew locations (mac)
 BREW ?= $(shell which brew)
+BREW := $(if $(BREW),$(BREW),$(HOME)/homebrew/bin/brew)
 
 BREW_ROOT ?= $(patsubst %/bin/,%,$(dir $(BREW)))
 BREW_ROOT := $(if $(BREW_ROOT),$(BREW_ROOT),$(HOME)/homebrew)
 NPM := $(if $(NPM),$(NPM),$(BREW_ROOT)/bin/npm)
-BOWER := $(if $(BOWER),$(BOWER),$(BREW_ROOT)/share/npm/bin/bower)
-VULCANIZE := $(if $(VULCANIZE),$(VULCANIZE),$(BREW_ROOT)/share/npm/bin/vulcanize)
+BOWER := $(if $(BOWER),$(BOWER),$(BREW_ROOT)/bin/bower)
+VULCANIZE := $(if $(VULCANIZE),$(VULCANIZE),$(BREW_ROOT)/bin/vulcanize)
+CHROME := $(if $(CHROME),$(CHROME),open -a "Google Chrome" --args)
 
 $(BREW):
 	git clone https://github.com/mxcl/homebrew.git $(BREW_ROOT)
